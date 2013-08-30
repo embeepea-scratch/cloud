4c697bd31329d81b7e067697f730197ac8fa4d89
Mark Phillips <embeepea@git>
2013-08-30 11:43:32 -0400

    move old README.md to drutils project as NAPPL.md; manually install Console_Table for drush

--- cloud1/puppet/site.pp
+++ cloud1/puppet/site.pp
@@ -118,7 +118,7 @@ class apache-vsites-server {
 
   class { "apache-server": }
   
-  file { ["/var/vsites", "/var/vsites/conf"] :
+  file { ["/var/vsites", "/var/vsites/conf", "/var/vsites/mysql"] :
     require => Class["git-server"],
     ensure => directory,
     owner  => "git",
@@ -200,7 +200,7 @@ package { 'php-pear':
 }
 
 exec { 'install-drush' :
-    command => '/usr/bin/pear channel-discover pear.drush.org ; /usr/bin/pear install drush/drush',
+    command => '/usr/bin/pear channel-discover pear.drush.org ; /usr/bin/pear install drush/drush ; cd /usr/share/pear/drush/lib ; /bin/mkdir tmp ; cd tmp ; /bin/tar xfz /etc/puppet/files/assets/drush-dependencies/Console_Table-1.1.3.tgz ; /bin/rm -f package.xml ; /bin/mv Console_Table-1.1.3 .. ; cd .. ; /bin/rm -rf tmp',
     unless => '/usr/bin/test -f /usr/bin/drush'
 }
 
